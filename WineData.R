d0c54107706a26dbf969f45b59f0a142e7100948
Dinup Kulangara <dinupkulangara@gmail.com>
2020-08-09 12:35:38 +0530

    Second Commit

--- WineData.R
+++ WineData.R
@@ -1,15 +1,20 @@
 library(corrplot)
 
 
-winequality <- read.csv("d/winequality-red.csv",header=T,sep=";")
+winequality <- read.csv("d/winequality-red.csv",header=T,sep=";",nrows=20)
 #winequality <- read.csv("D:/data/winequality/winequality-red.csv",header=T)
+class1<-sapply(winequality,class)
+class1
+typeof(class1)
+names(class1)
+#attr(class1)
 
-
-winequality
+winequality <- read.csv("d/winequality-red.csv",header=T,sep=";")
+#winequality
 #typeof(winequality)
-summary(winequality)
+#summary(winequality)
 winecorrel<-cor(winequality)
-corrplot(winecorrel, method="ellipse")
+#corrplot(winecorrel, method="ellipse")
 
-wineLR<-lm(winequality$quality~winequality$alcohol + winequality$citric.acid)
-print(summary(wineLR))
+#wineLR<-lm(winequality$quality~winequality$alcohol + winequality$citric.acid)
+#print(summary(wineLR))
